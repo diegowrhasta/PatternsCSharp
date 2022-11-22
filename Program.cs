068c8405265d0da73a30f0aa62511f95456dadaf
Diego Balderrama <diego.balderrama@sprbrk.com>
2022-11-22 13:34:17 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var list = new List<string>();
+var hello =
+    "The last question was asked for the first time, half in jest, on May 21, 2061, at a time when humanity first stepped " +
+    "into the light. The question came about as a result of a five dollar bet over highballs, and it happened this way: " +
+    "Alexander Adell and Bertram Lupov were two of the faithful attendants of Multivac. As well as any human";
+list.Add(hello);
+Console.WriteLine(string.Join(",", list));
